65a577fbf7da8dc888d7e5bafeb49eb9f9aa337f
Jon O'Brien <jvo7822@rit.edu>
2014-11-20 16:33:28 -0500

    added comments in environments.rb

--- EVE_Monitor/config/environment.rb
+++ EVE_Monitor/config/environment.rb
@@ -4,3 +4,9 @@ require 'reve'
 
 # Initialize the Rails application.
 EVEMonitor::Application.initialize!
+
+
+#need to setup devise for users and admin authentication
+#need to be able to setup api key injecting to be able to generate relevant unser info
+#from the servers for each independent user
+#devise_for :users
