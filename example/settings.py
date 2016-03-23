c50e6f8b88a39b1451562a10f223ea7e02337e83
Jon O'Brien <jvo7822@rit.edu>
2016-03-23 11:57:02 -0400

    more heroku dependency fixes

--- example/settings.py
+++ example/settings.py
@@ -147,3 +147,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+# needed for deployment `$ python manage.py collectstatic --noinput` 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
